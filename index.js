1d1b56590bad03cddc82d3d5523ce6ba078c851f
bdange <benjamin.dange@gmail.com>
2019-05-19 18:44:26 -0400

    new test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const terminalCommands = require("./terminal-command");
+const myLibrary = require("./my-library");
 
-terminalCommands.ls();
+// execute my-library here
+myLibrary.myFirstMethod();
+
+const terminalCommand = require("./terminal-command");
+
+terminalCommand.ls();
+terminalCommand.mkdir("sampleFolder");
+terminalCommand.touch("sampleFolder/sampleFile.txt");
