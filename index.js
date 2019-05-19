f115c46c52cb11769892a298b6e3d8cdfec2c0db
bdange <benjamin.dange@gmail.com>
2019-05-19 19:01:43 -0400

    improved test

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const myLibrary = require("./my-library");
-
-// execute my-library here
-myLibrary.myFirstMethod();
-
 const terminalCommand = require("./terminal-command");
 
 terminalCommand.ls();
